08337bd9718de3d03e62949a5bb653710510ae8f
celia <celia.f.194@gmail.com>
2020-06-08 21:18:14 +0200

    Engadir liña print Proba

--- ProyectoUbicua/src/proyectoubicua/ProyectoUbicua.java
+++ ProyectoUbicua/src/proyectoubicua/ProyectoUbicua.java
@@ -17,6 +17,8 @@ public class ProyectoUbicua {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Ola Mundo\n");
+        System.out.print("Proba\n");
+
     }
     
 }
