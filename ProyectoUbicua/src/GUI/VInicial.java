0b9de74b257047a689f28e705122f2c3da0420de
evinha <evinha@evinha-a-e5-573g>
2020-06-08 21:38:44 +0200

    Proba1

--- ProyectoUbicua/src/GUI/VInicial.java
+++ ProyectoUbicua/src/GUI/VInicial.java
@@ -9,12 +9,12 @@ package GUI;
  *
  * @author evinha
  */
-public class Proba extends javax.swing.JFrame {
+public class VInicial extends javax.swing.JFrame {
 
     /**
      * Creates new form Proba
      */
-    public Proba() {
+    public VInicial() {
         initComponents();
     }
 
@@ -60,20 +60,21 @@ public class Proba extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Proba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Proba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Proba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Proba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Proba().setVisible(true);
+                new VInicial().setVisible(true);
             }
         });
     }
