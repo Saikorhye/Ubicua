c10850841dfa112b8031d3a9d3197f3b4be15a93
felix <felix@DESKTOP-E6FFKCB>
2020-06-11 20:32:36 +0200

    Usuario Cambios en Multa

--- ProyectoUbicua/src/Clases/Main.java
+++ ProyectoUbicua/src/Clases/Main.java
@@ -7,7 +7,9 @@ package Clases;
 
 import GUI.IniciarSesion;
 import GUI.VInicial;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  *
@@ -24,19 +26,23 @@ public class Main {
         Usuario usu1 = new Usuario("Eva Suarez Vazquez", "eva@gmail.com", "1234", "Tarjeta", "666666666");
         Usuario usu2 = new Usuario("admin admin", "admin", "admin", "Tarjeta", "666666666");
      
-        //Multas
-        Multa mul = new Multa("Estacionamiento indebido en Rua do Franco del dia 13-05-2020", 45.84);
         //Notificaciones
         Notificacion noti = new Notificacion("Nueva multa!!", "Debe abonar el pago en la seccion de multas. \nPara ello haga click en 'Menu' > 'Multas', y una vez alli siga las instrucciones");
         //Vehiculos
         Vehiculo vehi1 = new Vehiculo("2332DDD", "Coche rojo");
         Vehiculo vehi2 = new Vehiculo("1655TXC", "Coche azul");
+        //Multas
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DATE, c.get(Calendar.DATE) -7);
+        
+        Multa mul = new Multa("Estacionamiento indebido en Rua do Franco", 45.84, new Timestamp(c.getTimeInMillis()), vehi1.getMatricula());
         
         //Anhadimos las notificaciones y multas a los usuarios
         usu1.addMulta(mul);
         usu1.addNotificacion(noti);
         usu2.addMulta(mul);
         usu2.addNotificacion(noti);
+        usu1.addVehiculo(vehi1);
         usu2.addVehiculo(vehi1);
         usu2.addVehiculo(vehi2);
         //Nuevos usuarios
